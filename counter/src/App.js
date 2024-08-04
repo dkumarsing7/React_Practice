3c9b5c25af508211ec85d74a649792bfd14bbb94
Deepak singh <67883350+dkumarsing7@users.noreply.github.com>
2024-08-04 21:26:45 +0530

    first file name changed to textArea and desiged a counter

---
+++ counter/src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [isCounting, setIsCounting] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if (isCounting) {
+      interval = setInterval(() => {
+        setCount(prevCount => prevCount + 1);
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [isCounting]);
+
+  const start = () => {
+    setIsCounting(true);
+  };
+  const stop = () => {
+    setIsCounting(false);
+    };
+    const reset = () => {
+      setCount(0);
+      setIsCounting(false);
+  };
+
+  return (
+    <>
+      <body className='d-flex justify-content-center align-items-center vh-100'>
+        <div className="container text-center">
+          <div className="my-3">
+            <h2>Counter: {count}</h2>
+          </div>
+          <button className='btn btn-success px-5 m-3' onClick={start}><h3>Start</h3></button>
+          <button className='btn btn-danger px-5 m-3' onClick={stop}><h3>Stop</h3></button>
+          <button className='btn btn-warning px-5 m-3' onClick={reset}><h3>Reset</h3></button>
+        </div>
+      </body>
+    </>
+  );
+}
+
+export default App;
